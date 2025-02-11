4b6ec670bb5e43fbd7fddf2af8bf378651d22fe8
Simon_R <symokevo@gmail.com>
2025-02-11 13:03:52 +0300

    Validate the Models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  has_many :borrowings, dependent: :destroy
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :published_year, presence: true, numeraciality: { only_integer: true }
+  validates :status, inclusion: { in: %w[available borrowed] }
 end
--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -1,3 +1,6 @@
 class Borrowing < ApplicationRecord
   belongs_to :book
+
+  validates :borrower_name, presence: true
+  validates :borrowed_at, presence: true
 end
