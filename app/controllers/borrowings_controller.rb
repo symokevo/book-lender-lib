b539e527f729740b22c1825db647ae91d48886c6
Simon_R <symokevo@gmail.com>
2025-02-11 13:36:13 +0300

    Implement the Borrowings Controller actions

--- app/controllers/borrowings_controller.rb
+++ app/controllers/borrowings_controller.rb
@@ -1,4 +1,7 @@
 class BorrowingsController < ApplicationController
   def index
+    @borrower_name = params[:borrower_name]
+    @borrowings = Borrowing.where(borrower_name: @borrower_name)
+      .order(borrowed_at: :desc)
   end
 end
